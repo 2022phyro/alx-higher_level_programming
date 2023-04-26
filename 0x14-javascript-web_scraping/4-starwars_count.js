6eb5bf6669c1f25fa6077525933526fccfac6952
2022phyro <phyrokelstein2@gmail.com>
2023-04-26 20:32:08 +0000

    4-starwars count.js. I used regex to compare the last number id to 18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const options = {
-  url: `${process.argv[2]}/`,
+  url: `${process.argv[2]}`,
   json: true
 };
 request(options, (error, response, body) => {
@@ -9,13 +9,13 @@ request(options, (error, response, body) => {
     console.log(error);
   }
   let count = 0;
-  for (let i = 0; i < body.results.length; i++) {
-    const show = body.results[i];
-    for (let j = 0; j < show.characters.length; j++) {
-      if (show.characters[j] === 'https://swapi-api.alx-tools.com/api/people/18/') {
+  body.results.forEach((curr) => {
+    curr.characters.forEach((mem) => {
+      const id = parseInt(mem.match(/\d+/)[0]);
+      if (id === 18) {
         count++;
       }
-    }
-  }
+    });
+  });
   console.log(count);
 });
