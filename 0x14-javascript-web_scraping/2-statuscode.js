fd0c2792fbbba1ad62692f248125d1d0d1b5127e
2022phyro <phyrokelstein2@gmail.com>
2023-04-26 13:34:18 +0000

    Setup

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const request = require('request')
-request(process.argv[2], (response) => {
-	console.log(response.statusCode);
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  console.log('code:', response.statusCode);
 });
